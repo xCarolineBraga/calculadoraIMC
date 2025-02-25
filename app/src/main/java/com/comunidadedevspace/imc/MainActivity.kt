4eb0d5d4d34534a94b5d257d2b5bb122bd3afcad
CarolineBraga <carolinebraga.guimaraes@gmail.com>
2025-02-25 22:22:24 +0000

    -implementação de classificação de IMC - segunda tela para demostrar resultado de IMC e classificação - edição de design dos botões, implementação de cores e icones

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.comunidadedevspace.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -16,13 +18,32 @@ class MainActivity : AppCompatActivity() {
         val btnCalcular = findViewById<Button>(R.id.btn_calcular)
 
         btnCalcular.setOnClickListener {
-            val peso: Float = edtPeso.text.toString().toFloat()
-            val altura: Float = edtAltura.text.toString().toFloat()
 
-            val resultado = peso / (altura * altura)
+            val pesoStr: String = edtPeso.text.toString()
+            val alturaStr: String = edtAltura.text.toString()
 
-            println("Ação botão "+ resultado)
-        }
+            if (pesoStr == "" || alturaStr == ""){
 
+                Snackbar.make(
+                    edtPeso,
+                    "Preencha todos os campos.",
+                    Snackbar.LENGTH_LONG
+                ).show()
+
+            } else {
+                val peso = pesoStr.toFloat()
+                val altura = alturaStr.toFloat()
+
+                val resultado = peso / (altura * altura)
+
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra(KEY_RESULT_IMC, resultado)
+                startActivity(intent)
+
+
+
+
+              }
+            }
     }
 }
---
+++ app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
@@ -0,0 +1,50 @@
+package com.comunidadedevspace.imc
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+const val KEY_RESULT_IMC = "ResultActivity.KEY_IMC"
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_result)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val result = intent.getFloatExtra(KEY_RESULT_IMC,0f)
+
+        val tvResult = findViewById<TextView>(R.id.tv_result)
+        val tvClassificacao = findViewById<TextView>(R.id.tv_classificacao)
+
+        tvResult.text = result.toString()
+
+        val classififcacao: String = if (result < 17f){
+            "DESNUTRIÇÃO"
+        } else if (result > 17f && result <= 18.49f){
+            "ABAIXO DO PESO"
+        } else if (result > 18.5f && result <= 24.99f){
+            "NORMAL"
+        } else if (result > 25f && result <= 29.99f){
+            "ACIMA DO PESO"
+        } else if (result > 30 && result <= 34.99f){
+            "OBESIDADE I"
+        } else if (result > 35f && result <= 39.99f){
+            "OBESIDADE II"
+        } else {
+            "OBESIDADE MÓRBIDA."
+        }
+        tvClassificacao.text = classififcacao
+
+
+
+
+    }
+}
